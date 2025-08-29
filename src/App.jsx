07cf342158c56c920ef982ccfc71ae7f47acdf3d
Muhammad Bilal Ahsan <hmbilal2024a@gmail.com>
2025-08-29 09:56:03 +0500

    updated folders and code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,6 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
 function App() {
-  return (
-    <>
-      {/* <NavBar></NavBar> */}
-      <Home></Home>
-    </>
-    // <Router>
-    //   <NavBar />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/projects" element={<Projects />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //   </Routes>
-    //   <Footer />
-    // </Router>
-  );
+  return <Home></Home>;
 }
 
 export default App;
